77bebfc64b86e6fe6a870f3cfe473bd8281dcf7a
Kavita Bavadiya <kavitabavadiya@gmail.com>
2023-09-04 22:59:48 +0530

    Update session.php

--- admin/session.php
+++ admin/session.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if(isset($_SESSION['id']) && $_SESSION['id'] == true){
+    if(isset($_SESSION['id']) && $_SESSION['id'] != "" ){
         header('location:dashboard.php');
         exit;
     }

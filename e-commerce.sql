90b846e885f553d85898c3592233da749b3a5522
vidz0509 <vairagbavadiya96@gmail.com>
2023-09-17 23:38:43 +0530

    updated db

--- e-commerce.sql
+++ e-commerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2023 at 12:50 PM
+-- Generation Time: Sep 17, 2023 at 08:08 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -29,15 +29,38 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `attribute` (
   `id` int(20) NOT NULL,
-  `category_id` int(20) NOT NULL,
+  `variants_id` int(20) NOT NULL,
   `attribute_name` varchar(20) NOT NULL,
-  `attribute_image` varchar(50) DEFAULT NULL,
+  `attribute_image` varchar(150) DEFAULT NULL,
   `created_by` int(20) DEFAULT NULL,
   `updated_by` int(11) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `is_active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `attribute`
+--
+
+INSERT INTO `attribute` (`id`, `variants_id`, `attribute_name`, `attribute_image`, `created_by`, `updated_by`, `created_at`, `updated_at`, `is_active`) VALUES
+(14, 6, 'M', NULL, 1, NULL, '2023-09-17 21:45:42', NULL, 1),
+(15, 6, 'S', NULL, 1, NULL, '2023-09-17 21:46:38', NULL, 1),
+(16, 6, 'XL', NULL, 1, NULL, '2023-09-17 21:46:50', NULL, 1),
+(17, 6, 'XXL', NULL, 1, NULL, '2023-09-17 21:47:03', NULL, 1),
+(18, 7, 'RED', '../attributes/images/Red.jpeg', 1, NULL, '2023-09-17 22:10:16', NULL, 1),
+(21, 7, 'BLUE', '../attributes/images/blue.jpeg', 1, NULL, '2023-09-17 22:13:01', NULL, 1),
+(22, 7, 'GREEN', '../attributes/images/green.png', 1, NULL, '2023-09-17 22:14:14', NULL, 1),
+(23, 7, 'BLACK', '../attributes/images/black.jpeg', 1, NULL, '2023-09-17 22:15:25', NULL, 1),
+(24, 7, 'WHITE', '../attributes/images/white.jpeg', 1, NULL, '2023-09-17 22:16:34', NULL, 1),
+(25, 8, 'Half Sleeves', '../attributes/images/HALF SLIVES .jpg', 1, NULL, '2023-09-17 22:22:03', NULL, 1),
+(26, 8, 'Full Sleeves', '../attributes/images/full shirt sleeves.jpg', 1, NULL, '2023-09-17 22:26:37', NULL, 1),
+(27, 8, 'Stripe Shirt', '../attributes/images/stripe shirt.jpg', 1, NULL, '2023-09-17 22:36:02', NULL, 1),
+(28, 8, 'Printed Shirt', '../attributes/images/printed shirt.jpg', 1, NULL, '2023-09-17 22:39:39', NULL, 1),
+(29, 8, ' Traditional Dress W', '../attributes/images/traditinal dress.jpg', 1, NULL, '2023-09-17 22:49:46', NULL, 1),
+(30, 8, 'Western Dress For Wo', '../attributes/images/WESTERN DRESS WOMEN.jpg', 1, NULL, '2023-09-17 22:55:31', NULL, 1),
+(31, 8, 'Office Wear Dress Fo', '../attributes/images/OFFICE WEAR DRESS.jpg', 1, NULL, '2023-09-17 23:00:01', NULL, 1),
+(32, 8, 'Formal wear Dress Fo', '../attributes/images/formal dress for women.jpg', 1, NULL, '2023-09-17 23:23:09', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -64,7 +87,6 @@ CREATE TABLE `category` (
 INSERT INTO `category` (`id`, `category_name`, `category_description`, `image`, `created_by`, `updated_by`, `created_at`, `updated_at`, `is_active`) VALUES
 (7, 'Men\'s Wear', '<p><strong>Well-dressed men understand that when they look like their most professional, put-together and capable selves, that the world relates to them accordingly and operates with them on a higher level. Clothing acts as the outward representation of their skills and personality, tying their positive attributes together into a comprehensive package. That&rsquo;s why regardless of whether or not they have an interest in fashion, attention is always paid to maintaining their image.</strong></p>', '../categories/images/81bEvE7vyLL._UX679_.jpg', 1, 1, '2023-09-15 23:38:36', '2023-09-15 23:39:32', 1),
 (8, 'Women\'s Wear', '<p><em><strong>is a budget-friendly clothing store that has colorful and trendy pieces for women. Their selection of graphic tees, sweatpants and joggers, and sneakers...</strong></em></p>', '../categories/images/71BMG8EorsL._UL1500_.jpg', 1, 1, '2023-09-15 23:45:32', '2023-09-15 23:45:32', 1),
-(9, 'Children\'s Wear', '<p><strong>Introducing our delightful collection of kids dresses, specially designed to make your little ones look adorable and stylish. Crafted with utmost care and attention to detail, these dresses are perfect for various occasions, from casual outings to special events. With a combination of comfort, quality, and trendy designs, our kids dresses are sure to be a hit among both children and parents alike.The freshness of the color will draw everybody&rsquo;s attention and your kid will be an instant hit! The dress is complete in itself and you can pair it nice accessory to break the perfect look. Breathable and comfortable wear experience in all seasons. This dress is perfect for a day out. This type of dress is known for its feminine and playful look. The dress is designed specifically for birthday parties and summer events, making it an excellent choice for parents who want their girl to look stylish and cute.</strong></p>', '../categories/images/41Np040iagL._UY741_.jpg', 1, 1, '2023-09-15 23:49:41', '2023-09-15 23:51:17', 1),
 (10, 'Boy\'s Wear', '<p><strong>Step up your Style Quotient by wearing this Casual Shirt from LEWEL. Team it up with your favourite pair of jeans, denim shorts and sneakers when you head out to work, party, vacation or anything your heart desires. It&rsquo;s your Life, your Rules and we will be all the way with you in style.</strong></p>', '../categories/images/61skoH0rMsL._UY741_.jpg', 1, 1, '2023-09-16 00:01:15', '2023-09-16 00:01:15', 1),
 (11, 'Girl\'s Wear', '<div id=\"title_feature_div\" class=\"celwidget\" data-feature-name=\"title\" data-csa-c-type=\"widget\" data-csa-c-content-id=\"title\" data-csa-c-slot-id=\"title_feature_div\" data-csa-c-asin=\"\" data-csa-c-is-in-initial-active-row=\"false\" data-csa-c-id=\"ktanjj-9k2v96-xnmk0v-atgnb1\" data-cel-widget=\"title_feature_div\">\r\n<div id=\"titleSection\" class=\"a-section a-spacing-none\">\r\n<h1 class=\"a-size-large a-spacing-none\"><span style=\"font-size: 18pt;\"><strong><span id=\"productTitle\" class=\"a-size-large product-title-word-break\">luyk Girls Floral Printed Top with <span style=\"font-size: 14pt;\">Trousers...</span></span></strong></span></h1>\r\n</div>\r\n</div>\r\n<div id=\"qpeTitleTag_feature_div\" class=\"celwidget\" data-feature-name=\"qpeTitleTag\" data-csa-c-type=\"widget\" data-csa-c-content-id=\"qpeTitleTag\" data-csa-c-slot-id=\"qpeTitleTag_feature_div\" data-csa-c-asin=\"\" data-csa-c-is-in-initial-active-row=\"false\" data-csa-c-id=\"f9408f-hapg7y-jfwjod-6m6qe8\" data-cel-widget=\"qpeTitleTag_feature_div\"></div>\r\n<div id=\"cmrsSummary_feature_div\" class=\"celwidget\" data-feature-name=\"cmrsSummary\" data-csa-c-type=\"widget\" data-csa-c-content-id=\"cmrsSummary\" data-csa-c-slot-id=\"cmrsSummary_feature_div\" data-csa-c-asin=\"\" data-csa-c-is-in-initial-active-row=\"false\" data-csa-c-id=\"cwdiip-prctva-86s4ps-ap479t\" data-cel-widget=\"cmrsSummary_feature_div\"></div>', '../categories/images/61OTdInzd9L._UY741_.jpg', 1, 1, '2023-09-16 00:06:16', '2023-09-16 00:28:33', 1);
 
@@ -173,8 +195,6 @@ INSERT INTO `user_type` (`id`, `type_name`) VALUES
 CREATE TABLE `variants` (
   `id` int(20) NOT NULL,
   `var_name` varchar(50) NOT NULL,
-  `var_image` varchar(100) DEFAULT NULL,
-  `price` int(50) NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `created_by` int(20) DEFAULT NULL,
@@ -186,8 +206,11 @@ CREATE TABLE `variants` (
 -- Dumping data for table `variants`
 --
 
-INSERT INTO `variants` (`id`, `var_name`, `var_image`, `price`, `created_at`, `updated_at`, `created_by`, `updated_by`, `is_active`) VALUES
-(5, 'testing1', '../variants/images/cartoon-5.png', 350, '2023-09-17 12:19:56', '2023-09-17 14:15:40', 1, 1, 1);
+INSERT INTO `variants` (`id`, `var_name`, `created_at`, `updated_at`, `created_by`, `updated_by`, `is_active`) VALUES
+(6, 'size', '2023-09-17 21:30:36', NULL, 1, NULL, 1),
+(7, 'color', '2023-09-17 21:30:58', NULL, 1, NULL, 1),
+(8, 'style & fit', '2023-09-17 21:31:25', NULL, 1, NULL, 1),
+(9, 'material', '2023-09-17 21:32:13', NULL, 1, NULL, 1);
 
 --
 -- Indexes for dumped tables
@@ -198,9 +221,9 @@ INSERT INTO `variants` (`id`, `var_name`, `var_image`, `price`, `created_at`, `u
 --
 ALTER TABLE `attribute`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_category_id` (`category_id`),
   ADD KEY `fk_att_updated_by` (`created_by`),
-  ADD KEY `fk_upd_by` (`updated_by`);
+  ADD KEY `fk_upd_by` (`updated_by`),
+  ADD KEY `fk_variants_id` (`variants_id`);
 
 --
 -- Indexes for table `category`
@@ -259,7 +282,7 @@ ALTER TABLE `variants`
 -- AUTO_INCREMENT for table `attribute`
 --
 ALTER TABLE `attribute`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -295,7 +318,7 @@ ALTER TABLE `user_type`
 -- AUTO_INCREMENT for table `variants`
 --
 ALTER TABLE `variants`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
@@ -306,8 +329,8 @@ ALTER TABLE `variants`
 --
 ALTER TABLE `attribute`
   ADD CONSTRAINT `fk_att_created_by` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_upd_by` FOREIGN KEY (`updated_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_upd_by` FOREIGN KEY (`updated_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_variants_id` FOREIGN KEY (`variants_id`) REFERENCES `variants` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `category`

1095f70903e4a480acfeceb984310971e7a438d6
vidz0509 <vairagbavadiya96@gmail.com>
2023-09-23 15:20:48 +0530

    update code

--- e-commerce.sql
+++ e-commerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 20, 2023 at 08:24 PM
+-- Generation Time: Sep 22, 2023 at 06:48 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
